98e056c94300e29ef00f0be25d6e0c6b30e3765a
145a <133004095+54145a@users.noreply.github.com>
2024-02-19 22:59:35 +0800

    .

--- daoscus.js
+++ daoscus.js
@@ -36,7 +36,6 @@ class Daoscus {
         like.classList.add("daoscus-comment-like");
         like.textContent = `顶(${comment.likeCount})`;
         commentContainer.appendChild(like);
-        console.log(comment.comment, comment.replyTo);
         if (!comment.replyTo) {
             const showReply = document.createElement("button");
             showReply.classList.add("daoscus-comment-show-reply");
